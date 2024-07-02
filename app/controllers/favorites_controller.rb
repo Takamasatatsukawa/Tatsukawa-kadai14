b4dce4cda817a12e875f7f8085a24f2518d0fe3e
tatsukawa <tatsukawatakamasa@gmail.com>
2024-07-02 18:34:12 +0900

    １回目

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,20 @@
 class FavoritesController < ApplicationController
 
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    @favorite = current_user.favorites.create(blog_id: params[:blog_id])
+    respond_to do |format|
+      format.html { redirect_to blogs_path }
+      format.js
+    end
   end
 
   def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    @favorite = current_user.favorites.find_by(id: params[:id])
+    @favorite.destroy if @favorite
+    respond_to do |format|
+      format.html { redirect_to blogs_path }
+      format.js
+    end
   end
 
 end
